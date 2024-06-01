aeffa1fdec5840709c9daa5ff44da7112050206e
Flemming <flemming.wu@gmail.com>
2024-05-31 22:21:57 -0700

    edit chrome config options, edit ecr workflow config

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import time
 from bs4 import BeautifulSoup
 import json
@@ -12,14 +10,20 @@ def lambda_handler(event, context) -> None:
     week_ago = (datetime.datetime.now() - datetime.timedelta(days=7)).date()
     # set up Chrome driver
     print('Settting up chrome driver...')
-    chrome_options = Options()
-    chrome_service = Service('/opt/chromedriver')
-    chrome_options.add_argument('--headless=new')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--single-process')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=chrome_options, service=chrome_service)
+    options = webdriver.ChromeOptions()
+    service = webdriver.ChromeService("/opt/chromedriver")
+
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument("--headless=new")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(options=options, service=service)
     print('Chrome driver set up successfully!')
     # begin webscraping
     print('Beginning webscraping...')
